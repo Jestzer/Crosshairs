731aa8e579ecca4bb5c98c846f1af615e60d6d5a
Jestzer <James@Buonopane.email>
2023-11-26 00:44:26 -0500

    First actual commit. Seems to work fine.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace Crosshairs
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Topmost = true;
+
+        }
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            
+            // Set the window style to allow click-through.
+            var hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+            WindowsServices.SetWindowExTransparent(hwnd);
+        }
+    }
+    public static class WindowsServices
+    {
+        private const int WS_EX_TRANSPARENT = 0x20;
+
+        [DllImport("user32.dll")]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll")]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        public static void SetWindowExTransparent(IntPtr hwnd)
+        {
+            var extendedStyle = GetWindowLong(hwnd, -20);
+            SetWindowLong(hwnd, -20, extendedStyle | WS_EX_TRANSPARENT);
+        }
+    }
+}
