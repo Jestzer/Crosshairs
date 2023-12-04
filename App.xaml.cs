8e04ce4bb5f3bf4dd9b40fbf8d418aa45532d525
Jestzer <James@Buonopane.email>
2023-12-03 19:14:13 -0500

    The coordinates can now be changed and saved. Need to prevent invalid coordinates from being saved.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,17 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
 namespace Crosshairs
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            InterfaceWindow interfaceWindow = new InterfaceWindow();
+            interfaceWindow.Show();
 
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+
+        }
+    }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Globalization;
+using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Data;
 
 namespace Crosshairs
 {
@@ -34,6 +36,22 @@ namespace Crosshairs
         {
             var extendedStyle = GetWindowLong(hwnd, -20);
             SetWindowLong(hwnd, -20, extendedStyle | WS_EX_TRANSPARENT);
+        }
+    }
+    public class MarginConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 2 && values[0] is double x && values[1] is double y)
+            {
+                return new Thickness(x, y, 0, 0);
+            }
+            return new Thickness(0);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
         }
     }
 }
